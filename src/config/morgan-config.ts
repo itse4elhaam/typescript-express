c98482eae542c500a8694f75b2066d94fb792a2a
Elhaam Basheer Chaudhry <e4elhaam@gmail.com>
2024-02-13 17:19:28 +0500

    feat: added colors for logs

--- src/config/morgan-config.ts
+++ src/config/morgan-config.ts
@@ -1,52 +1,59 @@
 import morgan from "morgan";
-import chalk from "chalk";
+import colors from "colors"; // Import colors library
 import { REQ_COLORS } from "../lib/constants";
 import { Request } from "express";
 import { HttpVerbs } from "../lib/types";
 
-morgan.token('params', (req: Request, res) => {
-    return JSON.stringify(req.params);
+morgan.token("params", (req: Request, res) => {
+  return JSON.stringify(req.params);
 });
 
-morgan.token('query', (req: Request, res) => {
-    return JSON.stringify(req.query);
+morgan.token("query", (req: Request, res) => {
+  return JSON.stringify(req.query);
 });
 
-morgan.token('body', (req: Request, res) => {
-    return JSON.stringify(req.body);
+morgan.token("body", (req: Request, res) => {
+  return JSON.stringify(req.body);
 });
 
-morgan.token('colored-status', (req: Request, res) => {
-    const status = res.statusCode;
-    const color = status >= 500 ? 'red' : status >= 400 ? 'yellow' : status >= 300 ? 'cyan' : 'green';
-    return chalk[color](status);
+morgan.token("colored-status", (req: Request, res) => {
+  const status = res.statusCode;
+  const color =
+    status >= 500
+      ? "red"
+      : status >= 400
+      ? "yellow"
+      : status >= 300
+      ? "cyan"
+      : "green";
+  return colors[color](status.toString()); 
 });
 
-morgan.token('colored-method', (req, res) => {
-    const method: HttpVerbs = req.method as HttpVerbs;
-    const color: string = REQ_COLORS[method];
-    // doing this temporarily
-    // @ts-expect-error
-    return chalk[color](method);
+morgan.token("colored-method", (req, res) => {
+  const method: HttpVerbs = req.method as HttpVerbs;
+  const color: string = REQ_COLORS[method];
+  // this is temporary
+  // @ts-expect-error
+  return colors[color](method.toString()); 
 });
 
 const morganMiddleware = morgan(
-    (tokens, req, res) => {
-        const method = tokens['colored-method'](req, res);
-        const url = tokens.url(req, res);
-        const status = tokens['colored-status'](req, res);
-        const contentLength = tokens.res(req, res, 'content-length');
-        const responseTime = tokens['response-time'](req, res);
-        const params = tokens.params(req, res);
-        const query = tokens.query(req, res);
-        const body = tokens.body(req, res);
+  (tokens, req, res) => {
+    const method = tokens["colored-method"](req, res);
+    const url = tokens.url(req, res);
+    const status = tokens["colored-status"](req, res);
+    const contentLength = tokens.res(req, res, "content-length");
+    const responseTime = tokens["response-time"](req, res);
+    const params = tokens.params(req, res);
+    const query = tokens.query(req, res);
+    const body = tokens.body(req, res);
 
-        return `\n${method} ${url} ${status} ${contentLength} - ${responseTime} ms: \n- Params: ${params} \n- Query: ${query} \n- Body: ${body}\n`;
-    },
-    {
-        // Stream for morgan logs (you can customize this as needed)
-        stream: process.stdout,
-    }
+    return `\n${method} ${url} ${status} ${contentLength} - ${responseTime} ms: \n- Params: ${params} \n- Query: ${query} \n- Body: ${body}\n`;
+  },
+  {
+    // Stream for morgan logs (you can customize this as needed)
+    stream: process.stdout,
+  }
 );
 
 export default morganMiddleware;
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,23 @@
 import express, { Response } from "express";
 import cors from "cors";
-// const apiRoutes = require("./routes/index");
-// const morganMiddleware = require("./config/morgan-config");
+// import apiRoutes from "./routes/index" ;
+import morganMiddleware from "./config/morgan-config";
 require("dotenv").config();
 const app = express();
 app.use(cors());
-// app.use(morganMiddleware);
+app.use(morganMiddleware);
 
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (res: Response) => {
-	res.send("Hello from your Maps Api server!");
+app.get("/", (req, res: Response) => {
+  res.send("Hello from your Maps Api server!");
 });
 
 app.listen(port, () => {
-	console.log(`http://localhost:${port}/`);
+  console.log(`http://localhost:${port}/`);
 });
 
 // app.use("/api", apiRoutes);
